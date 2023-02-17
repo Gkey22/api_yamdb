0dc5cae44c4bd33ccc81cfc3676e0ee7cc8e59f4
Gkey22 <111044692+Gkey22@users.noreply.github.com>
2023-02-17 14:04:50 +0700

    модель отзыв/комент

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,34 +1,56 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
+from .validators import validate_year
 
-class Categorie(models.Model): 
-    name = models.CharField(max_length=256) 
-    slug = models.SlugField(unique=True) 
- 
-    def __str__(self): 
-        return self.name 
+User = get_user_model()
 
 
-class Genre(models.Model): 
-    name = models.CharField(max_length=256) 
-    slug = models.SlugField(unique=True) 
- 
-    def __str__(self): 
-        return self.name 
+class Review(models.Model):
+    """Модель отзывов."""
+    text = models.TextField(verbose_name='Текст отзыва')
+    title = models.ForeignKey(Title,
+                              verbose_name='Произведение',
+                              on_delete=models.CASCADE,
+                              related_name='reviews')
+    score = models.IntegerField(validators=[MinValueValidator(1),
+                                            MaxValueValidator(10)],
+                                verbose_name='Оценка',
+                                null=True)
+    pub_date = models.DateTimeField('Дата публикации',
+                                    auto_now_add=True)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               verbose_name='Автор отзыва')
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        constraints = (models.UniqueConstraint(fields=('title', 'author'),
+                                               name='unique_review'),)
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return self.text
 
 
-class Title(models.Model): 
-    name = models.CharField(max_length=256)
-    year = models.PositiveIntegerField(blank=False)
-    description = models.CharField(max_length=256) 
-    genre = models.ForeignKey(
-        Genre, on_delete=models.SET_NULL, blank=False,
-        null=True, related_name='genres'
-    )
-    category = models.ForeignKey(
-        Categorie, on_delete=models.SET_NULL, blank=False,
-        null=True, related_name='categories'
-    )
+class Comment(models.Model):
+    """Модель комментариев."""
+    text = models.TextField(max_length=200,
+                            verbose_name='Комментарий к отзыву')
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               verbose_name='Автор комментария')
+    pub_date = models.DateTimeField(verbose_name='Дата публикации',
+                                    auto_now_add=True)
+    review = models.ForeignKey(Review,
+                               on_delete=models.CASCADE,
+                               related_name='comments')
 
-    def __str__(self): 
-        return self.name 
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return self.text
