4cb67740253b8c6e67a75b9704a4b332d68c0c33
Igor <borny@bk.ru>
2023-02-21 15:24:09 +0700

    Manually addet some models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model 
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
@@ -45,3 +46,52 @@ class GenreTitle(models.Model):
     genre = models.ForeignKey(
         Genre, on_delete=models.CASCADE, related_name='genres'
     )
+
+
+class Review(models.Model):
+    """Модель отзывов."""
+    text = models.TextField(verbose_name='Текст отзыва')
+    title = models.ForeignKey(Title,
+                              verbose_name='Произведение',
+                              on_delete=models.CASCADE,
+                              related_name='reviews')
+    score = models.IntegerField(validators=[MinValueValidator(1),
+                                            MaxValueValidator(10)],
+                                verbose_name='Оценка',
+                                null=True)
+    pub_date = models.DateTimeField('Дата публикации',
+                                    auto_now_add=True)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               verbose_name='Автор отзыва')
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        constraints = (models.UniqueConstraint(fields=('title', 'author'),
+                                               name='unique_review'),)
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return self.text
+
+
+class Comment(models.Model):
+    """Модель комментариев."""
+    text = models.TextField(max_length=200,
+                            verbose_name='Комментарий к отзыву')
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               verbose_name='Автор комментария')
+    pub_date = models.DateTimeField(verbose_name='Дата публикации',
+                                    auto_now_add=True)
+    review = models.ForeignKey(Review,
+                               on_delete=models.CASCADE,
+                               related_name='comments')
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return self.text
